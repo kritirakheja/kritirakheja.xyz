e684c2c78879bd05057fd2fe1d9e62d58505ab1d
Kriti Rakheja <kritirakheja18@gmail.com>
2023-05-10 23:21:52 +0530

    chore: fix typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <div className="mx-auto max-w-7xl">
         <div className="max-w-md">
           <h1 className="text-4xl font-bold">Hey, I am Kriti 👋</h1>
-          <p className="mt-4 text-xl leading-relaxed">A psychology enthusiast turned product strategist. With a passion for innovation and a knack for learning, I've spent the past few years immersing myself in the world of startups, tech, and product development.</p>
+          <p className="mt-4 text-xl leading-relaxed">A psychology enthusiast turned product strategist. With a passion for innovation and a knack for learning, I&apos;ve spent the past few years immersing myself in the world of startups, tech, and product development.</p>
           <p className="mt-6">Associate Product Manager at <a className="border-b-2 border-black" href="https://dosteducation.com">Dost Education</a></p>
         </div>
       </div>
