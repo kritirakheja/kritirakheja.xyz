b726d4e10a0d3dcb2322e41a6bf6a2a02cdacec9
Nitesh Kumar Niranjan <hello@theux.dev>
2023-05-11 17:08:28 +0530

    chore: fix font load

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme")
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Circular Std', ...defaultTheme.fontFamily.sans],
-      }
+        sans: ["CircularStd", ...defaultTheme.fontFamily.sans],
+      },
     },
   },
   plugins: [],
